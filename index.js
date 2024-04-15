9deb019138ed62e14b855d9db7007b58472d69fa
Nicolas Seine <nicolasseine.dev@gmail.com>
2024-04-15 18:51:17 +0200

    add static express middleware

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.use(express.json());
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :data ")
 );
+app.use(express.static("dist"));
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
 
 let data = [
