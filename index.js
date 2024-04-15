d16049eee4cd0761150d7a784854b8c5d56b1868
Nicolas Seine <nicolasseine.dev@gmail.com>
2024-04-15 18:21:14 +0200

    complete exercice till 3.8

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
+morgan.token("data", (req) => JSON.stringify(req.body));
 
+// Use middlewares
+app.use(cors());
 app.use(express.json());
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :data ")
+);
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
 
-const data = [
+let data = [
   {
     id: 1,
     name: "Arto Hellas",
@@ -35,4 +43,51 @@ app.get("/api/persons", (req, res) => {
 
 app.get("/info", (req, res) => {
   res.send(`Phonebook has info for ${data.length} people <br/> ${new Date()}`);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = data.find((person) => person.id === id);
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  if (!data.find((person) => person.id === id)) {
+    console.log("Person with id", id, "does not exist");
+    return res.status(404).end();
+  }
+  data = data.filter((person) => person.id !== id);
+  console.log("Person with id", id, "has been deleted");
+  console.log(data);
+  res.status(204).end();
+});
+
+app.post("/api/persons", (req, res) => {
+  const body = req.body;
+  if (!body.name || !body.number) {
+    return res.status(400).json({
+      error:
+        "content missing. PLease check that both name and number are provided",
+    });
+  }
+  if (data.find((person) => person.name === body.name)) {
+    return res.status(400).json({
+      error: "name already exist in the phonebook. Name must be unique",
+    });
+  }
+
+  const newperson = {
+    id: Math.floor(Math.random() * 1000),
+    name: body.name,
+    number: body.number,
+  };
+
+  data.push(newperson);
+  console.log(data);
+  res.status(201).json(body);
 });
